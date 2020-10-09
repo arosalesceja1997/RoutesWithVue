8d44081251511dae1782ad0fc7caf3c7f4da643c
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-10-09 18:05:37 -0500

    Anthony - data

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Services from '../views/Services.vue'
+import Photos from '../views/Photos.vue';
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/services',
+        name: 'Services',
+        component: Services,
+    },
+    {
+        path: '/photos',
+        name: 'Photos',
+        component: Photos,
+    },
+    {
+        path: '/photos/:id',
+        name: 'PhotosId',
+        component: Photos,
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/Photos.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Galeria</h1>
+        <router-link :to="{ name: 'PhotosId', params: {id: photo} }" 
+            v-for="(photo, index) of photos" :key="index">
+            <button>
+                {{photo}}
+            </button>
+        </router-link>
+        <Photo/>
+        <button @click="previous">Anterios</button>
+        <button @click="home">Home</button>
+        <button @click="next">Next</button>
+    </div>
+</template>
+
+<script>
+import Photo from '@/components/Photo.vue';
+export default {
+    components:{
+        Photo,
+    },
+    data(){
+        return {
+            photos: [1,2,3,4],
+        }
+    },
+    methods: {
+        home(){
+            this.$router.push('/');
+        },
+        previous(){
+            this.$router.go(-1);
+        },
+        next(){
+            this.$router.go(1);
+        }
+    }
+}
+</script>
